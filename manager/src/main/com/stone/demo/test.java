399d74b0b0f8e9f1e4f13bf94688f08cec7f158a
chenchaohai <979839467@qq.com>
2016-03-19 15:14:48 +0800

    first commit

---
+++ manager/src/main/com/stone/demo/test.java
@@ -0,0 +1,14 @@
+package main.com.stone.demo;
+
+/**
+ * Created by chenchen on 2016/3/19.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("首次提交");
+    }
+
+    private void dd(){
+        System.out.println("dfd");
+    }
+}
